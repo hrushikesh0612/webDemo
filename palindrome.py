aac3fe0ff01193ae68a523ba69b8234e35b55eea
hrushi <hrushiketpw22@gmail.com>
2025-06-11 23:40:27 +0530

    separated project

--- palindrome.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(num):
-    return str(num) == str(num)[::-1]
-
-number = int(input("Enter a number: "))
-if is_palindrome(number):
-    print(f"{number} is a palindrome.")
-else:
-    print(f"{number} is not a palindrome.")
